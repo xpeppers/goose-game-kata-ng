236a228701b29becbf5b3662d0aad9bcfecec4d6
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2016-12-14 12:49:51 +0100

    refactors goose game class to improve readability

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 class GooseGame {
   constructor () {
     this.players = []
-  //  this.positions = []
   }
   addPlayer (name) {
-    if (~this.players.map(player => player.name).indexOf(name)) {
+    if (this.players.some(byName(name))) {
       return `${name}: giocatore già presente`
     }
     this.players.push({'name': name, 'position': 0})
     return `Giocatori: ${this.players.map(player => player.name).join(', ')}`
   }
   movePlayer (name, firstDie, secondDie) {
-    let player = this.players.find(player => player.name === name)
+    let player = this.players.find(byName(name))
     let positionLabel = player.position || 'partenza'
     player.position += (firstDie + secondDie)
     return `${name} tira ${firstDie}, ${secondDie}. ${player.name} muove da ${positionLabel} a ${player.position}`
+  }
+}
+
+function byName (name) {
+  return function (player) {
+    return name === player.name
   }
 }
 
